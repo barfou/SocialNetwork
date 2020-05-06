cdffadbdf1c5b42fa6b689a468d167f6931c9fb0
PaulFgx <pfourgoux@live.fr>
2020-05-06 11:29:19 +0200

    init firebase

--- app/src/main/java/fr/barfou/socialnetwork/MainActivity.kt
+++ app/src/main/java/fr/barfou/socialnetwork/MainActivity.kt
@@ -2,11 +2,17 @@ package fr.barfou.socialnetwork
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val activitiesRef = Firebase.database.reference.child("Activities")
+        var idTask = activitiesRef.push().key!!
+        activitiesRef.child(idTask).setValue(true)
     }
 }
