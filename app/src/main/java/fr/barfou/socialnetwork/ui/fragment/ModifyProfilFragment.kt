e504766d33b815dbdb57ae4a4ac1a69edb3924ff
ClementB01 <bar.clement01@gmail.com>
2020-05-13 08:42:23 +0200

    feature OK at 80%, need to add a button and add +1 to mouth

--- app/src/main/java/fr/barfou/socialnetwork/ui/fragment/ModifyProfilFragment.kt
+++ app/src/main/java/fr/barfou/socialnetwork/ui/fragment/ModifyProfilFragment.kt
@@ -1,5 +1,6 @@
 package fr.barfou.socialnetwork.ui.fragment
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,12 +9,19 @@ import androidx.fragment.app.Fragment
 import fr.barfou.socialnetwork.ui.activity.LoginActivity
 import fr.barfou.socialnetwork.R
 import fr.barfou.socialnetwork.ui.adapter.TrophyAdapter
-import kotlinx.android.synthetic.main.fragment_profil.*
+import kotlinx.android.synthetic.main.fragment_modify_profil.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.*
+import java.util.Calendar.*
 
 
 class ModifyProfilFragment: Fragment() {
 
-    private lateinit var trophyAdapter: TrophyAdapter
+    val c = getInstance()
+    var year : Int? = null
+    var month : Int? = null
+    var day : Int? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -28,6 +36,34 @@ class ModifyProfilFragment: Fragment() {
         (activity as? LoginActivity)?.supportActionBar?.apply {
             this.setTitle(R.string.title_fragment_modify_profil)
             this.setDisplayHomeAsUpEnabled(false)
+        }
+
+
+
+        dtpBirthday.setOnClickListener {
+            if(etBirthday.text.toString() == "") {
+                day = c.get(DAY_OF_MONTH)
+                month = c.get(MONTH)
+                year = c.get(YEAR)
+            } else {
+                val tblDate : List<String> = etBirthday.text.toString().split("/")
+
+                day = tblDate[0].toInt()
+                month = tblDate[1].toInt()
+                year = tblDate[2].toInt()
+            }
+
+            val dpd = DatePickerDialog(
+                this.requireContext(),
+
+                DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+
+                // Display Selected date in textbox
+                etBirthday.setText(dayOfMonth.toString() + "/"  + monthOfYear.toString() + "/" + year.toString())
+
+            }, year!!, month!!, day!!)
+
+            dpd.show()
         }
     }
 }
