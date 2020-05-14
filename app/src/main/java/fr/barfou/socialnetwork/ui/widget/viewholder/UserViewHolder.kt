d1c9ed6210a620a40134f97b9635674450671d90
PaulFgx <pfourgoux@live.fr>
2020-05-14 14:25:36 +0200

    show user initials ok

--- app/src/main/java/fr/barfou/socialnetwork/ui/widget/viewholder/UserViewHolder.kt
+++ app/src/main/java/fr/barfou/socialnetwork/ui/widget/viewholder/UserViewHolder.kt
@@ -6,11 +6,13 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import fr.barfou.socialnetwork.R
 import fr.barfou.socialnetwork.data.model.User
+import kotlinx.android.synthetic.main.holder_user.view.*
 
 class UserViewHolder private constructor(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun bind(model: User) {
         itemView.apply {
+            tv_user_pseudo.text = model.getInitials()
         }
     }
 
