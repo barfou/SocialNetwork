47ed9520391b5860b044d7663be0d0350fdca110
PaulFgx <pfourgoux@live.fr>
2020-05-18 07:51:03 +0200

    implement get location in fragment register

--- app/src/main/java/fr/barfou/socialnetwork/ui/fragment/RegisterFragment.kt
+++ app/src/main/java/fr/barfou/socialnetwork/ui/fragment/RegisterFragment.kt
@@ -1,31 +1,50 @@
 package fr.barfou.socialnetwork.ui.fragment
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Location
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
 import androidx.navigation.fragment.findNavController
+import com.google.android.gms.location.*
 import com.google.firebase.auth.FirebaseAuth
 import fr.barfou.socialnetwork.ui.activity.LoginActivity
 import fr.barfou.socialnetwork.R
+import fr.barfou.socialnetwork.data.model.ConvertedLocation
 import fr.barfou.socialnetwork.data.model.User
 import fr.barfou.socialnetwork.ui.activity.MainActivity
+import fr.barfou.socialnetwork.ui.utils.convertLatLongToLocation
 import fr.barfou.socialnetwork.ui.utils.getCurrentDate
 import fr.barfou.socialnetwork.ui.utils.hide
 import fr.barfou.socialnetwork.ui.utils.show
 import fr.barfou.socialnetwork.ui.viewmodel.LoginViewModel
+import kotlinx.android.synthetic.main.activity_location.*
 import kotlinx.android.synthetic.main.fragment_login.btnLogin
 import kotlinx.android.synthetic.main.fragment_login.btnRegister
 import kotlinx.android.synthetic.main.fragment_login.etPassword
 import kotlinx.android.synthetic.main.fragment_register.*
 
 class RegisterFragment : Fragment() {
+
+    val PERMISSION_ID = 42
+    lateinit var mFusedLocationClient: FusedLocationProviderClient
+    lateinit var addresses : List<Address>
 
     private lateinit var loginViewModel: LoginViewModel
     private lateinit var auth: FirebaseAuth
@@ -34,6 +53,7 @@ class RegisterFragment : Fragment() {
         super.onCreate(savedInstanceState)
         activity?.run {
             loginViewModel = ViewModelProvider(this, LoginViewModel).get()
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
         } ?: throw IllegalStateException("Invalid Activity")
     }
 
@@ -90,6 +110,108 @@ class RegisterFragment : Fragment() {
             } else {
                 progress_bar.hide()
                 Toast.makeText(requireContext(), "Saisie incorrecte.", Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+
+                mFusedLocationClient.lastLocation.addOnCompleteListener(this.requireActivity()) { task ->
+                    var location: Location? = task.result
+                    if (location == null) {
+                        requestNewLocationData()
+                    } else {
+                        try {
+
+                            /*geocoder = Geocoder(this)
+
+                            addresses = geocoder.getFromLocation(location.latitude, location.longitude, 1)
+
+                            val city = addresses[0].locality
+                            val country = addresses[0].countryName*/
+
+                            val myLocation : ConvertedLocation = convertLatLongToLocation(this.requireActivity(), location.latitude, location.longitude)
+
+                            val town = myLocation.town
+                            val country = myLocation.country
+
+                            tvLatLong.text = location.latitude.toString() + " / " + location.longitude.toString()
+                            tvLocation.text = town + " / " + country
+                        } catch (e: Exception) {
+                            println(e.toString())
+                        }
+                    }
+                }
+            } else {
+                Toast.makeText(this.requireActivity(), "Turn on location", Toast.LENGTH_LONG).show()
+                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                startActivity(intent)
+            }
+        } else {
+            requestPermissions()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNewLocationData() {
+        val mLocationRequest = LocationRequest()
+        mLocationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        mLocationRequest.interval = 0
+        mLocationRequest.fastestInterval = 0
+        mLocationRequest.numUpdates = 1
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this.requireActivity())
+        mFusedLocationClient!!.requestLocationUpdates(
+                mLocationRequest, mLocationCallback,
+                Looper.myLooper()
+        )
+    }
+
+    private val mLocationCallback = object : LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            val mLastLocation: Location = locationResult.lastLocation
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager: LocationManager = requireActivity().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun checkPermissions(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                        this.requireActivity(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(
+                        this.requireActivity(),
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    private fun requestPermissions() {
+        ActivityCompat.requestPermissions(
+                this.requireActivity(),
+                arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSION_ID
+        )
+    }
+
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_ID) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getLastLocation()
             }
         }
     }
